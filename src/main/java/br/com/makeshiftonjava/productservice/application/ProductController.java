3e40fda66a9449ac93c496255044f7d0ba995599
Vinicius.saraiva Acc <vinicius.saraiva.acc@accurate.com.br>
2018-01-08 03:40:21 -0200

    First commit

---
+++ src/main/java/br/com/makeshiftonjava/productservice/application/ProductController.java
@@ -0,0 +1,23 @@
+package br.com.makeshiftonjava.productservice.application;
+
+import br.com.makeshiftonjava.productservice.model.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(("/products"))
+public class ProductController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductController.class);
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Product> retrieveProduct(@PathVariable Long id) {
+        LOG.info("/reviews called, processing time: {}");
+        return ResponseEntity.ok(new Product("Teste"));
+    }
+}
---
+++ src/main/java/br/com/makeshiftonjava/productservice/model/Product.java
@@ -0,0 +1,18 @@
+package br.com.makeshiftonjava.productservice.model;
+
+public class Product {
+
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
